b1739ad6ed44e6bf0cf7d16a04bd4b1a6ec6ca0e
haha <971757231@qq.com>
2019-12-11 00:17:01 +0800

    first

---
+++ src/main/java/com/tmall/Controller/CategoryController.java
@@ -0,0 +1,68 @@
+package com.tmall.Controller;
+
+
+import com.github.pagehelper.PageHelper;
+import com.github.pagehelper.PageInfo;
+import com.tmall.pojo.Category;
+import com.tmall.service.CategoryService;
+import com.tmall.util.ImageUtil;
+import com.tmall.util.Page;
+import com.tmall.util.UploadedImageFile;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import javax.imageio.ImageIO;
+import javax.servlet.http.HttpSession;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
+import java.util.List;
+
+@Controller
+@RequestMapping("")
+public class CategoryController {
+    @Autowired
+    CategoryService categoryService;
+    @RequestMapping("/admin_category_list")
+    public String list(Model model, Page page){
+        PageHelper.offsetPage(page.getStart(),page.getCount());
+        List<Category> cs= categoryService.list();
+        int total = (int) new PageInfo<>(cs).getTotal();
+        page.setTotal(total);
+        model.addAttribute("cs", cs);
+        model.addAttribute("page", page);
+        return "admin/listCategory";
+
+
+    }
+
+    @RequestMapping("admin_category_add")
+    public String add(Category c, HttpSession session, UploadedImageFile uploadedImageFile) throws IOException {
+        System.out.println(c.getId());
+        categoryService.add(c);
+        System.out.println(c.getId());
+        File  imageFolder= new File(session.getServletContext().getRealPath("img/category"));
+        File file = new File(imageFolder,c.getId()+".jpg");
+        if(!file.getParentFile().exists())
+            file.getParentFile().mkdirs();
+        System.out.println(uploadedImageFile);
+        System.out.println(uploadedImageFile.getImage());
+        System.out.println(file);
+        uploadedImageFile.getImage().transferTo(file);
+        BufferedImage img = ImageUtil.change2jpg(file);
+        ImageIO.write(img, "jpg", file);
+
+        return "redirect:/admin_category_list";
+    }
+
+    @RequestMapping("admin_category_delete")
+    public String delete(int id,HttpSession session) throws IOException{
+        categoryService.delete(id);
+        File imageFolder = new File(session.getServletContext().getRealPath("img/category"));
+        File file = new File(imageFolder,id+".jpg");
+        file.delete();
+        return "redirect:/admin_category_list";
+    }
+}
